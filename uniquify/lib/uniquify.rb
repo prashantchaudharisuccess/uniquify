f4ecb645af77912372c17e5f8b0c72a72f034987
prashantchaudharisuccess <prashant.b.chaudhari@gmail.com>
2017-12-11 16:22:18 +0530

    Update uniquify.rb

--- uniquify/lib/uniquify.rb
+++ uniquify/lib/uniquify.rb
@@ -1,7 +1,1 @@
-module Uniquify
-  require 'fog'
-  def self.uniquify(access_token, secret, name)
-    connection = Fog::Storage.new({ :provider => 'AWS', :aws_access_key_id => access_token, :aws_secret_access_key => secret})
-    directory = connection.directories.create(:key => "#{name}", :public => true)
-  end
-end
+
